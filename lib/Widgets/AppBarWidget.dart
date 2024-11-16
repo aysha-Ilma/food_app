8ce81204fd3ec2dbc1599dddc9e669a09f773601
aysha-ilma <mmayshailma@gmail.com>
2024-11-16 06:16:39 +0530

    create notifications Icon

--- lib/Widgets/AppBarWidget.dart
+++ lib/Widgets/AppBarWidget.dart
@@ -19,9 +19,38 @@ class AppBarWidget extends StatelessWidget {
             child: Container(
               padding: EdgeInsets.all(8),
               decoration: BoxDecoration(
+                color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color:Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0,3),
+                  )
+                ]
               ),
               child: Icon(CupertinoIcons.bars),
+            ),
+          ),
+
+          InkWell(
+            onTap:() {},
+            child: Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color:Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0,3),
+                  )
+                ]
+              ),
+              child: Icon(Icons.notifications),
             ),
           ),
       ]),
