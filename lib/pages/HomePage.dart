583d5f8ac16f32a45b76a325836d13c03a4272fc
aysha-ilma <mmayshailma@gmail.com>
2024-11-16 08:01:46 +0530

    search Icon container

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -8,41 +8,57 @@ class Homepage extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-
           //Custom App Bar widget
           AppBarWidget(),
 
           //Search
-          Padding(padding: EdgeInsets.symmetric(
-            vertical: 10, 
-            horizontal: 15,
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 15,
             ),
             child: Container(
               width: double.infinity,
               height: 50,
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow (
-                    color : Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 10,
-                    offset: Offset(0,3),
-                  ),
-                ]
-                ),
-                child: Padding(padding: EdgeInsets.symmetric(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ]),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
                   horizontal: 10,
                 ),
-                child: Row( children: [
-                  Icon(CupertinoIcons.search,
-                  color: Colors.red,
-                  ),
-                ],),
+                child: Row(
+                  children: [
+                    Icon(
+                      CupertinoIcons.search,
+                      color: Colors.red,
+                    ),
+                    Container(
+                        height: 50,
+                        width: 300,
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                        ),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "what would you like to have?",
+                          ),
+                          ),
+                        ))
+                  ],
                 ),
               ),
-              ),
+            ),
+          ),
         ],
       ),
     );
