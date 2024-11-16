875bdf75fb92b4b7aecf339e1df2bc65616fed5d
aysha-ilma <mmayshailma@gmail.com>
2024-11-16 07:46:49 +0530

    create search Icon

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/Widgets/AppBarWidget.dart';
 
@@ -7,8 +8,41 @@ class Homepage extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
+
           //Custom App Bar widget
           AppBarWidget(),
+
+          //Search
+          Padding(padding: EdgeInsets.symmetric(
+            vertical: 10, 
+            horizontal: 15,
+            ),
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow (
+                    color : Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0,3),
+                  ),
+                ]
+                ),
+                child: Padding(padding: EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                child: Row( children: [
+                  Icon(CupertinoIcons.search,
+                  color: Colors.red,
+                  ),
+                ],),
+                ),
+              ),
+              ),
         ],
       ),
     );
